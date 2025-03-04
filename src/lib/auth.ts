3b3ce341f43da8e89c1b7d0f01ff7d83dff301dd
Darshan Patel <ddpatel97@gmail.com>
2025-03-04 18:47:49 -0330

    feat: authentication with authjs

---
+++ src/lib/auth.ts
@@ -0,0 +1,46 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import NextAuth from 'next-auth';
+
+import Github from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
+
+import { prisma } from './db';
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google,
+    Github,
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async signIn({ user }) {
+      // Check if the user exists in the database
+      if (!user.id) {
+        return false;
+      }
+      const existingUser = await prisma.user.findUnique({
+        where: { id: user.id },
+      });
+
+      // If the user exists, update lastLogin
+      if (existingUser) {
+        await prisma.user.update({
+          where: { id: user.id },
+          data: {
+            lastLogin: new Date(),
+          },
+        });
+      }
+
+      // Return true regardless of whether the user was found or not
+      return true;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  jwt: {
+    maxAge: 60 * 60 * 24 * 30,
+  },
+});
